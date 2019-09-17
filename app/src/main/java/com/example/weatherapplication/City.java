27a8b4e072fb9df1f8d6ef08b587e73e3d587770
Sergei_Smirnov <ser2603@yandex.ru>
2019-09-18 02:49:05 +0500

    added model of weather

---
+++ app/src/main/java/com/example/weatherapplication/City.java
@@ -0,0 +1,49 @@
+package com.example.weatherapplication;
+
+import com.example.whatistheweather.modelWeather.WeatherRequest;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class City implements Serializable {
+
+    private String cityName;
+    private WeatherRequest weatherRequest;
+    private static ArrayList<City> cityArrayList = new ArrayList();
+    private static Map<String, WeatherRequest> cityMap = new HashMap<>();
+
+    public City(String cityName){
+        this.cityName = cityName;
+        weatherRequest = new WeatherRequest();
+        weatherRequest.setName(cityName);
+        cityArrayList.add(this);
+    }
+
+    public City(String cityName, WeatherRequest weatherRequest){
+        this.cityName = cityName;
+        this.weatherRequest = weatherRequest;
+        cityArrayList.add(this);
+        cityMap.put(cityName, weatherRequest);
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public WeatherRequest getWeatherRequest() { return weatherRequest; }
+
+    public void putWeatherRequest(WeatherRequest weatherRequest) {
+        this.weatherRequest = weatherRequest;
+    }
+
+    public static ArrayList<City> getCityArrayList() {
+        return cityArrayList;
+    }
+
+    public static Map<String, WeatherRequest> getCityMap() {
+        return cityMap;
+    }
+}
---
+++ app/src/main/java/com/example/weatherapplication/modelWeather/Wind.java
@@ -0,0 +1,22 @@
+package com.example.weatherapplication.modelWeather;
+
+public class Wind {
+    private float speed;
+    private float deg;
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public float getDeg() {
+        return deg;
+    }
+
+    public void setDeg(int deg) {
+        this.deg = deg;
+    }
+}

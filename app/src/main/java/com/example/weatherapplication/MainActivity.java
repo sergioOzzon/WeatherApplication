82d1240990c1ccc9a573bacfd19cfd3de38efd31
Sergei_Smirnov <ser2603@yandex.ru>
2019-09-18 05:10:41 +0500

    Added Default SettingActivity and empty WeatherFragment

--- app/src/main/java/com/example/weatherapplication/MainActivity.java
+++ app/src/main/java/com/example/weatherapplication/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 updateWeather();
             }
         });
+        updateWeather();
     }
 
     private void updateWeather() {
--- app/src/main/java/com/example/weatherapplication/WeatherFragment.java
+++ app/src/main/java/com/example/weatherapplication/WeatherFragment.java
@@ -1,6 +1,5 @@
 package com.example.weatherapplication;
 
-
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -9,41 +8,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WeatherFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class WeatherFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private static final String ARG_CURRENT_CITY = "currentCity";
 
+    private City city;
 
     public WeatherFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment WeatherFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static WeatherFragment newInstance(String param1, String param2) {
+    public static WeatherFragment newInstance(City city) {
         WeatherFragment fragment = new WeatherFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putSerializable(ARG_CURRENT_CITY, city);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,8 +29,7 @@ public class WeatherFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            city = (City) getArguments().getSerializable(ARG_CURRENT_CITY);
         }
     }
 
